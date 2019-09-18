ba03d6dca5b6eaed6e6ffddb147c003e25b15d24
Sergey Koshkin <koshkin.sergey@gmail.com>
2019-09-18 17:32:26 +0300

    STM32F7xx Driver: Added EXTI driver

--- Device/STMicroelectronics/STM32F7xx/Driver/EXTI_STM32F7xx.c
+++ Device/STMicroelectronics/STM32F7xx/Driver/EXTI_STM32F7xx.c
@@ -280,14 +280,6 @@ void RTC_Alarm_IRQHandler(void)
   EXTI_IRQHandler(EXTI_RTC_ALARM);
 }
 
-#if defined(STM32F401xC) || defined(STM32F401xE) ||                                                 \
-    defined(STM32F405xx) || defined(STM32F415xx) || defined(STM32F407xx) || defined(STM32F417xx) || \
-    defined(STM32F411xE) ||                                                                         \
-    defined(STM32F412Cx) || defined(STM32F412Rx) || defined(STM32F412Vx) || defined(STM32F412Zx) || \
-    defined(STM32F413xx) ||                                                                         \
-    defined(STM32F423xx) || defined(STM32F427xx) || defined(STM32F429xx) || defined(STM32F437xx) || \
-    defined(STM32F439xx) || defined(STM32F446xx) || defined(STM32F469xx) || defined(STM32F479xx)
-
 /**
  * @fn          void OTG_FS_WKUP_IRQHandler(void)
  * @brief       USB OTG FS Wakeup through EXTI line
@@ -296,12 +288,6 @@ void OTG_FS_WKUP_IRQHandler(void)
 {
   EXTI_IRQHandler(EXTI_USB_OTG_FS_WAKEUP);
 }
-
-#endif
-
-#if defined(STM32F407xx) || defined(STM32F417xx) ||                                                 \
-    defined(STM32F427xx) || defined(STM32F429xx) || defined(STM32F437xx) ||                         \
-    defined(STM32F439xx) || defined(STM32F469xx) || defined(STM32F479xx)
 
 /**
  * @fn          void ETH_WKUP_IRQHandler(void)
@@ -312,12 +298,6 @@ void ETH_WKUP_IRQHandler(void)
   EXTI_IRQHandler(EXTI_ETHERNET_WAKEUP);
 }
 
-#endif
-
-#if defined(STM32F405xx) || defined(STM32F415xx) || defined(STM32F407xx) || defined(STM32F417xx) || \
-    defined(STM32F427xx) || defined(STM32F429xx) || defined(STM32F437xx) ||                         \
-    defined(STM32F439xx) || defined(STM32F446xx) || defined(STM32F469xx) || defined(STM32F479xx)
-
 /**
  * @fn          void OTG_HS_WKUP_IRQHandler(void)
  * @brief       USB OTG HS Wakeup through EXTI
@@ -326,8 +306,6 @@ void OTG_HS_WKUP_IRQHandler(void)
 {
   EXTI_IRQHandler(EXTI_USB_OTG_HS_WAKEUP);
 }
-
-#endif
 
 /**
  * @fn          void EXTI0_IRQHandler(void)
