8b16a52b93b0f572f66a19de92e35bc67d9bd5b6
Sergey Koshkin <koshkin.sergey@gmail.com>
2024-03-14 22:55:17 +0300

    RISC-V Core: Added functions

--- Include/Core/Riscv/gcc.h
+++ Include/Core/Riscv/gcc.h
@@ -101,4 +101,122 @@
   #define __INTERRUPT                            __attribute__((interrupt))
 #endif
 
+
+/* ########################  Core Instruction Access  ####################### */
+
+/**
+ * @brief   No Operation
+ * @details No Operation does nothing. This instruction can be used for code alignment purposes.
+ */
+#define __NOP()                             __ASM volatile ("nop")
+
+/**
+ * @brief   Reverse byte order (32 bit)
+ * @details Reverses the byte order in unsigned integer value. For example, 0x12345678 becomes 0x78563412.
+ * @param [in]    value  Value to reverse
+ * @return               Reversed value
+ */
+__STATIC_FORCEINLINE
+uint32_t __REV(uint32_t value)
+{
+  return (__builtin_bswap32(value));
+}
+
+/**
+ * @brief   Reverse byte order (16 bit)
+ * @details Reverses the byte order in a 16-bit value and returns the signed 16-bit result. For example, 0x0080 becomes 0x8000.
+ * @param [in]    value  Value to reverse
+ * @return               Reversed value
+ */
+__STATIC_FORCEINLINE
+int16_t __REVSH(int16_t value)
+{
+  return ((int16_t)__builtin_bswap16(value));
+}
+
+/**
+ * @brief   Rotate Right in unsigned value (32 bit)
+ * @details Rotate Right (immediate) provides the value of the contents of a register rotated by a variable number of bits.
+ * @param [in]    op1  Value to rotate
+ * @param [in]    op2  Number of Bits to rotate
+ * @return               Rotated value
+ */
+__STATIC_FORCEINLINE
+uint32_t __ROR(uint32_t op1, uint32_t op2)
+{
+  op2 %= 32U;
+  if (op2 == 0U) {
+    return (op1);
+  }
+  return ((op1 >> op2) | (op1 << (32U - op2)));
+}
+
+/**
+ * @brief   Reverse bit order of value
+ * @details Reverses the bit order of the given value.
+ * @param [in]    value  Value to reverse
+ * @return               Reversed value
+ */
+__STATIC_FORCEINLINE
+uint32_t __RBIT(uint32_t value)
+{
+  uint32_t result;
+  int32_t s = (sizeof(value) * 8U) - 1U; /* extra shift needed at end */
+
+  result = value;                      /* r will be reversed bits of v; first get LSB of v */
+  for (value >>= 1U; value; value >>= 1U) {
+    result <<= 1U;
+    result |= value & 1U;
+    s--;
+  }
+  result <<= s;                        /* shift when v's highest bits are zero */
+
+  return (result);
+}
+
+/**
+ * @brief       Reverse bit order of value
+ * @details     Reverses the bit order of the given value.
+ * @param[in]   value  Value to reverse
+ * @return      Reversed value
+ */
+__STATIC_FORCEINLINE uint16_t __RBIT16(uint16_t value)
+{
+  uint16_t result;
+  int32_t s = (sizeof(value) * 8U) - 1U; /* extra shift needed at end */
+
+  result = value;                      /* r will be reversed bits of v; first get LSB of v */
+  for (value >>= 1U; value; value >>= 1U) {
+    result <<= 1U;
+    result |= value & 1U;
+    s--;
+  }
+  result <<= s;                        /* shift when v's highest bits are zero */
+
+  return (result);
+}
+
+/**
+ * @brief   Count leading zeros
+ * @param [in]  value  Value to count the leading zeros
+ * @return             number of leading zeros in value
+ */
+__STATIC_FORCEINLINE
+uint8_t __CLZ(uint32_t value)
+{
+  /* Even though __builtin_clz produces a CLZ instruction on ARM, formally
+     __builtin_clz(0) is undefined behavior, so handle this case specially.
+     This guarantees ARM-compatible results if happening to compile on a non-ARM
+     target, and ensures the compiler doesn't decide to activate any
+     optimizations using the logic "value was passed to __builtin_clz, so it
+     is non-zero".
+     ARM GCC 7.3 and possibly earlier will optimize this test away, leaving a
+     single CLZ instruction.
+   */
+  if (value == 0U) {
+    return (32U);
+  }
+  return (__builtin_clz(value));
+}
+
 #endif /* __GCC_H */
