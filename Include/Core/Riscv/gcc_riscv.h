54eaad5224efe82b6e82553606a26b416d09f7be
Sergey Koshkin <koshkin.sergey@gmail.com>
2024-03-18 16:27:06 +0300

    RISC-V Core: Added CSR macros.

--- Include/Core/Riscv/gcc_riscv.h
+++ Include/Core/Riscv/gcc_riscv.h
@@ -15,90 +15,166 @@
  * limitations under the License.
  */
 
-#ifndef __GCC_H
-#define __GCC_H
+#ifndef __GCC_RISCV_H
+#define __GCC_RISCV_H
 
-/* CMSIS compiler specific defines */
+#include <stdint.h>
 
+#ifdef __cplusplus
+ extern "C" {
+#endif
+
+/* NMSIS compiler specific defines */
+/** \brief Pass information from the compiler to the assembler. */
 #ifndef   __ASM
   #define __ASM                                  __asm
 #endif
+
+/** \brief Recommend that function should be inlined by the compiler. */
 #ifndef   __INLINE
   #define __INLINE                               inline
 #endif
-#ifndef   __FORCEINLINE
-  #define __FORCEINLINE                          __attribute__((always_inline))
-#endif
+
+/** \brief Define a static function that may be inlined by the compiler. */
 #ifndef   __STATIC_INLINE
   #define __STATIC_INLINE                        static inline
 #endif
+
+/** \brief Define a static function that should be always inlined by the compiler. */
 #ifndef   __STATIC_FORCEINLINE
   #define __STATIC_FORCEINLINE                   __attribute__((always_inline)) static inline
 #endif
+
+/** \brief Inform the compiler that a function does not return. */
 #ifndef   __NO_RETURN
   #define __NO_RETURN                            __attribute__((__noreturn__))
 #endif
+
+/** \brief Inform that a variable shall be retained in executable image. */
 #ifndef   __USED
   #define __USED                                 __attribute__((used))
 #endif
+
+/** \brief restrict pointer qualifier to enable additional optimizations. */
 #ifndef   __WEAK
   #define __WEAK                                 __attribute__((weak))
 #endif
+
+/** \brief specified the vector size of the variable, measured in bytes */
+#ifndef   __VECTOR_SIZE
+  #define __VECTOR_SIZE(x)                       __attribute__((vector_size(x)))
+#endif
+
+/** \brief Request smallest possible alignment. */
 #ifndef   __PACKED
   #define __PACKED                               __attribute__((packed, aligned(1)))
 #endif
+
+/** \brief Request smallest possible alignment for a structure. */
 #ifndef   __PACKED_STRUCT
   #define __PACKED_STRUCT                        struct __attribute__((packed, aligned(1)))
 #endif
+
+/** \brief Request smallest possible alignment for a union. */
 #ifndef   __PACKED_UNION
   #define __PACKED_UNION                         union __attribute__((packed, aligned(1)))
 #endif
+
 #ifndef   __UNALIGNED_UINT16_WRITE
   #pragma GCC diagnostic push
   #pragma GCC diagnostic ignored "-Wpacked"
   #pragma GCC diagnostic ignored "-Wattributes"
-  __PACKED_STRUCT T_UINT16_WRITE { uint16_t v; };
+  /** \brief Packed struct for unaligned uint16_t write access */
+  __PACKED_STRUCT T_UINT16_WRITE {
+      uint16_t v;
+  };
   #pragma GCC diagnostic pop
+  /** \brief Pointer for unaligned write of a uint16_t variable. */
   #define __UNALIGNED_UINT16_WRITE(addr, val)    (void)((((struct T_UINT16_WRITE *)(void *)(addr))->v) = (val))
 #endif
+
 #ifndef   __UNALIGNED_UINT16_READ
   #pragma GCC diagnostic push
   #pragma GCC diagnostic ignored "-Wpacked"
   #pragma GCC diagnostic ignored "-Wattributes"
-  __PACKED_STRUCT T_UINT16_READ { uint16_t v; };
+  /** \brief Packed struct for unaligned uint16_t read access */
+  __PACKED_STRUCT T_UINT16_READ {
+      uint16_t v;
+  };
   #pragma GCC diagnostic pop
+  /** \brief Pointer for unaligned read of a uint16_t variable. */
   #define __UNALIGNED_UINT16_READ(addr)          (((const struct T_UINT16_READ *)(const void *)(addr))->v)
 #endif
+
 #ifndef   __UNALIGNED_UINT32_WRITE
   #pragma GCC diagnostic push
   #pragma GCC diagnostic ignored "-Wpacked"
   #pragma GCC diagnostic ignored "-Wattributes"
-  __PACKED_STRUCT T_UINT32_WRITE { uint32_t v; };
+  /** \brief Packed struct for unaligned uint32_t write access */
+  __PACKED_STRUCT T_UINT32_WRITE {
+      uint32_t v;
+  };
   #pragma GCC diagnostic pop
+  /** \brief Pointer for unaligned write of a uint32_t variable. */
   #define __UNALIGNED_UINT32_WRITE(addr, val)    (void)((((struct T_UINT32_WRITE *)(void *)(addr))->v) = (val))
 #endif
+
 #ifndef   __UNALIGNED_UINT32_READ
   #pragma GCC diagnostic push
   #pragma GCC diagnostic ignored "-Wpacked"
   #pragma GCC diagnostic ignored "-Wattributes"
-  __PACKED_STRUCT T_UINT32_READ { uint32_t v; };
+  /** \brief Packed struct for unaligned uint32_t read access */
+  __PACKED_STRUCT T_UINT32_READ {
+      uint32_t v;
+  };
   #pragma GCC diagnostic pop
+  /** \brief Pointer for unaligned read of a uint32_t variable. */
   #define __UNALIGNED_UINT32_READ(addr)          (((const struct T_UINT32_READ *)(const void *)(addr))->v)
 #endif
+
+/** \brief Minimum `x` bytes alignment for a variable. */
 #ifndef   __ALIGNED
   #define __ALIGNED(x)                           __attribute__((aligned(x)))
 #endif
+
+/** \brief restrict pointer qualifier to enable additional optimizations. */
 #ifndef   __RESTRICT
   #define __RESTRICT                             __restrict
 #endif
+
+/** \brief Barrier to prevent compiler from reordering instructions. */
 #ifndef   __COMPILER_BARRIER
   #define __COMPILER_BARRIER()                   __ASM volatile("":::"memory")
 #endif
-/**
- *  @brief Use this attribute to indicate that the specified function is an interrupt handler.
- */
+
+/** \brief provide the compiler with branch prediction information, the branch is usually true */
+#ifndef   __USUALLY
+  #define __USUALLY(exp)                         __builtin_expect((exp), 1)
+#endif
+
+/** \brief provide the compiler with branch prediction information, the branch is rarely true */
+#ifndef   __RARELY
+  #define __RARELY(exp)                          __builtin_expect((exp), 0)
+#endif
+
+/** \brief Use this attribute to indicate that the specified function is an interrupt handler run in Machine Mode. */
 #ifndef   __INTERRUPT
   #define __INTERRUPT                            __attribute__((interrupt))
+#endif
+
+/** \brief Use this attribute to indicate that the specified function is an interrupt handler run in Machine Mode. */
+#ifndef   __MACHINE_INTERRUPT
+  #define __MACHINE_INTERRUPT                    __attribute__ ((interrupt ("machine")))
+#endif
+
+/** \brief Use this attribute to indicate that the specified function is an interrupt handler run in Supervisor Mode. */
+#ifndef   __SUPERVISOR_INTERRUPT
+  #define __SUPERVISOR_INTERRUPT                 __attribute__ ((interrupt ("supervisor")))
+#endif
+
+/** \brief Use this attribute to indicate that the specified function is an interrupt handler run in User Mode. */
+#ifndef   __USER_INTERRUPT
+  #define __USER_INTERRUPT                       __attribute__ ((interrupt ("user")))
 #endif
 
 
@@ -228,35 +304,126 @@ uint8_t __CLZ(uint32_t value)
 
 /* #########################  Core Function Access  ######################### */
 
-#define read_csr(reg) (                                                        \
-{                                                                              \
+#define __STR(s)                                 #s
+#define STRINGIFY(s)                             __STR(s)
+
+/**
+ * @brief       CSR operation Macro for csrr instruction.
+ * @details     Read the content of csr register and return it
+ * @param       csr  CSR macro definition
+ * @return      CSR register value
+ */
+#define CSR_READ(csr)                                                          \
+({                                                                             \
   uint32_t __tmp;                                                              \
-  __ASM volatile ("csrr %0, " #reg : "=r"(__tmp));                             \
+  __ASM volatile ("csrr %0, " STRINGIFY(csr)                                   \
+                : "=r"(__tmp)                                                  \
+                :                                                              \
+                : "memory");                                                   \
   __tmp;                                                                       \
 })
 
-#define write_csr(reg, val)                                                    \
-  __ASM volatile ("csrw " #reg ", %0" :: "Kr"(val))
+/**
+ * @brief       CSR operation Macro for csrw instruction.
+ * @details     Write the content of val to csr register
+ * @param       csr  CSR macro definition
+ * @param       val  value to store into the CSR register
+ */
+#define CSR_WRITE(csr, val)                                                    \
+({                                                                             \
+  __ASM volatile ("csrw " STRINGIFY(csr) ", %0"                                \
+                :                                                              \
+                : "Kr"(val)                                                    \
+                : "memory");                                                   \
+})
 
-#define swap_csr(reg, val) (                                                   \
-{                                                                              \
+/**
+ * @brief       CSR operation Macro for csrrw instruction.
+ * @details     Read the content of csr register to __tmp,
+ *              then write content of val into csr register,
+ *              then return __tmp
+ * @param       csr  CSR macro definition
+ * @param       val  value to store into the CSR register
+ * @return      CSR register value before written
+ */
+#define CSR_SWAP(csr, val)                                                     \
+({                                                                             \
   uint32_t __tmp;                                                              \
-  __ASM volatile ("csrrw %0, " #reg ", %1" : "=r"(__tmp) : "Kr"(val));         \
+  __ASM volatile ("csrrw %0, " STRINGIFY(csr) ", %1"                           \
+                : "=r"(__tmp)                                                  \
+                : "Kr"(val)                                                    \
+                : "memory");                                                   \
   __tmp;                                                                       \
 })
 
-#define set_csr(reg, bit) (                                                    \
-{                                                                              \
+/**
+ * @brief       CSR operation Macro for csrs instruction.
+ * @details     Set csr register to be csr_content | val
+ * @param       csr  CSR macro definition
+ * @param       val  Mask value to be used wih csrs instruction
+ */
+#define CSR_SET(csr, val)                                                      \
+({                                                                             \
+  __ASM volatile ("csrs " STRINGIFY(csr) ", %0"                                \
+                :                                                              \
+                : "Kr"(val)                                                    \
+                : "memory");                                                   \
+})
+
+/**
+ * @brief       CSR operation Macro for csrrs instruction.
+ * @details     Read the content of csr register to __tmp,
+ *              then set csr register to be csr_content | val,
+ *              then return __tmp
+ * @param       csr  CSR macro definition
+ * @param       val  Mask value to be used wih csrrs instruction
+ * @return      CSR register value before written
+ */
+#define CSR_READ_SET(csr, val)                                                 \
+({                                                                             \
   uint32_t __tmp;                                                              \
-  __ASM volatile ("csrrs %0, " #reg ", %1" : "=r"(__tmp) : "Kr"(bit));         \
+  __ASM volatile ("csrrs %0, " STRINGIFY(csr) ", %1"                           \
+                : "=r"(__tmp)                                                  \
+                : "Kr"(val)                                                    \
+                : "memory");                                                   \
   __tmp;                                                                       \
 })
 
-#define clear_csr(reg, bit) (                                                  \
-{                                                                              \
+/**
+ * @brief       CSR operation Macro for csrc instruction.
+ * @details     Set csr register to be csr_content & ~val
+ * @param       csr  CSR macro definition
+ * @param       val  Mask value to be used wih csrc instruction
+ */
+#define CSR_CLEAR(csr, val)                                                    \
+({                                                                             \
+  __ASM volatile ("csrc " STRINGIFY(csr) ", %0"                                \
+                :                                                              \
+                : "Kr"(val)                                                    \
+                : "memory");                                                   \
+})
+
+/**
+ * @brief       CSR operation Macro for csrrc instruction.
+ * @details     Read the content of csr register to __tmp,
+ *              then set csr register to be csr_content & ~val,
+ *              then return __tmp
+ * @param       csr  CSR macro definition
+ * @param       val  Mask value to be used wih csrrc instruction
+ * @return      CSR register value before written
+ */
+#define CSR_READ_CLEAR(csr, val)                                               \
+({                                                                             \
   uint32_t __tmp;                                                              \
-  __ASM volatile ("csrrc %0, " #reg ", %1" : "=r"(__tmp) : "Kr"(bit));         \
+  __ASM volatile ("csrrc %0, " STRINGIFY(csr) ", %1"                           \
+                : "=r"(__tmp)                                                  \
+                : "Kr"(val)                                                    \
+                : "memory");                                                   \
   __tmp;                                                                       \
 })
 
-#endif /* __GCC_H */
+#ifdef __cplusplus
+}
+#endif
+
+#endif /* __GCC_RISCV_H */
--- Include/Core/Riscv/icc_riscv.h
+++ Include/Core/Riscv/icc_riscv.h
@@ -1,5 +1,5 @@
 /*
- * Copyright (C) 2023 Sergey Koshkin <koshkin.sergey@gmail.com>
+ * Copyright (C) 2023-2024 Sergey Koshkin <koshkin.sergey@gmail.com>
  * All rights reserved
  *
  * Licensed under the Apache License, Version 2.0 (the License); you may
@@ -15,9 +15,181 @@
  * limitations under the License.
  */
 
-#ifndef __ICCRISCV_H
-#define __ICCRISCV_H
+#ifndef __ICC_RISCV_H
+#define __ICC_RISCV_H
+
+#include <stdint.h>
+
+#ifdef __cplusplus
+ extern "C" {
+#endif
+
+/* Compiler specific defines */
+
+/** \brief Pass information from the compiler to the assembler. */
+#ifndef   __ASM
+  #define __ASM                                  __asm
+#endif
+
+/** \brief Recommend that function should be inlined by the compiler. */
+#ifndef   __INLINE
+  #define __INLINE                               inline
+#endif
+
+/** \brief Define a static function that may be inlined by the compiler. */
+#ifndef   __STATIC_INLINE
+  #define __STATIC_INLINE                        static inline
+#endif
+
+/** \brief Define a static function that should be always inlined by the compiler. */
+#ifndef   __STATIC_FORCEINLINE
+  #define __STATIC_FORCEINLINE                   __attribute__((always_inline)) static inline
+#endif
+
+/** \brief Inform the compiler that a function does not return. */
+#ifndef   __NO_RETURN
+  #define __NO_RETURN                            __attribute__((__noreturn__))
+#endif
+
+/** \brief Inform that a variable shall be retained in executable image. */
+#ifndef   __USED
+  #define __USED                                 __attribute__((used))
+#endif
+
+/** \brief restrict pointer qualifier to enable additional optimizations. */
+#ifndef   __WEAK
+  #define __WEAK                                 __attribute__((weak))
+#endif
+
+/** \brief specified the vector size of the variable, measured in bytes, not supported in IAR */
+#ifndef   __VECTOR_SIZE
+  #define __VECTOR_SIZE(x)
+#endif
+
+/** \brief Request smallest possible alignment. */
+#ifndef   __PACKED
+  #define __PACKED                               __attribute__((packed, aligned(1)))
+#endif
+
+/** \brief Request smallest possible alignment for a structure. */
+#ifndef   __PACKED_STRUCT
+  #define __PACKED_STRUCT                        struct __attribute__((packed, aligned(1)))
+#endif
+
+/** \brief Request smallest possible alignment for a union. */
+#ifndef   __PACKED_UNION
+  #define __PACKED_UNION                         union __attribute__((packed, aligned(1)))
+#endif
+
+#ifndef   __UNALIGNED_UINT16_WRITE
+  #pragma language=save
+  #pragma language=extended
+  /** \brief Packed struct for unaligned uint16_t write access */
+  __PACKED_STRUCT T_UINT16_WRITE {
+      uint16_t v;
+  };
+  #pragma language=restore
+  /** \brief Pointer for unaligned write of a uint16_t variable. */
+  #define __UNALIGNED_UINT16_WRITE(addr, val)    (void)((((struct T_UINT16_WRITE *)(void *)(addr))->v) = (val))
+#endif
+
+#ifndef   __UNALIGNED_UINT16_READ
+  #pragma language=save
+  #pragma language=extended
+  /** \brief Packed struct for unaligned uint16_t read access */
+  __PACKED_STRUCT T_UINT16_READ {
+      uint16_t v;
+  };
+  #pragma language=restore
+  /** \brief Pointer for unaligned read of a uint16_t variable. */
+  #define __UNALIGNED_UINT16_READ(addr)          (((const struct T_UINT16_READ *)(const void *)(addr))->v)
+#endif
+
+#ifndef   __UNALIGNED_UINT32_WRITE
+  #pragma language=save
+  #pragma language=extended
+  /** \brief Packed struct for unaligned uint32_t write access */
+  __PACKED_STRUCT T_UINT32_WRITE {
+      uint32_t v;
+  };
+  #pragma language=restore
+  /** \brief Pointer for unaligned write of a uint32_t variable. */
+  #define __UNALIGNED_UINT32_WRITE(addr, val)    (void)((((struct T_UINT32_WRITE *)(void *)(addr))->v) = (val))
+#endif
+
+#ifndef   __UNALIGNED_UINT32_READ
+  #pragma language=save
+  #pragma language=extended
+  /** \brief Packed struct for unaligned uint32_t read access */
+  __PACKED_STRUCT T_UINT32_READ {
+      uint32_t v;
+  };
+  #pragma language=restore
+  /** \brief Pointer for unaligned read of a uint32_t variable. */
+  #define __UNALIGNED_UINT32_READ(addr)          (((const struct T_UINT32_READ *)(const void *)(addr))->v)
+#endif
+
+/** \brief Minimum `x` bytes alignment for a variable. */
+#ifndef   __ALIGNED
+  #define __ALIGNED(x)                           __attribute__((aligned(x)))
+#endif
+
+/** \brief restrict pointer qualifier to enable additional optimizations. */
+#ifndef   __RESTRICT
+  #define __RESTRICT                             __restrict
+#endif
+
+/** \brief Barrier to prevent compiler from reordering instructions. */
+#ifndef   __COMPILER_BARRIER
+  #define __COMPILER_BARRIER()                   __ASM volatile("":::"memory")
+#endif
+
+/** \brief provide the compiler with branch prediction information, the branch is usually true */
+#ifndef   __USUALLY
+  #define __USUALLY(exp)                         (exp)
+#endif
+
+/** \brief provide the compiler with branch prediction information, the branch is rarely true */
+#ifndef   __RARELY
+  #define __RARELY(exp)                          (exp)
+#endif
+
+/** \brief Use this attribute to indicate that the specified function is an interrupt handler run in Machine Mode. */
+#ifndef   __INTERRUPT
+  #define __INTERRUPT                            __machine __interrupt
+#endif
+
+/** \brief Use this attribute to indicate that the specified function is an interrupt handler run in Machine Mode. */
+#ifndef   __MACHINE_INTERRUPT
+  #define __MACHINE_INTERRUPT                    __machine __interrupt
+#endif
+
+/** \brief Use this attribute to indicate that the specified function is an interrupt handler run in Supervisor Mode. */
+#ifndef   __SUPERVISOR_INTERRUPT
+  #define __SUPERVISOR_INTERRUPT                 __supervisor __interrupt
+#endif
+
+/** \brief Use this attribute to indicate that the specified function is an interrupt handler run in User Mode. */
+#ifndef   __USER_INTERRUPT
+  #define __USER_INTERRUPT                       __user __interrupt
+#endif
 
 
+/* #########################  Core Function Access  ######################### */
 
-#endif /* __ICCRISCV_H */
+#include <intrinsics.h>
+
+#define CSR_SWAP         __write_csr
+#define CSR_READ         __read_csr
+#define CSR_WRITE        __write_csr
+#define CSR_READ_SET     __set_bits_csr
+#define CSR_SET          __set_bits_csr
+#define CSR_READ_CLEAR   __clear_bits_csr
+#define CSR_CLEAR        __clear_bits_csr
+
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif /* __ICC_RISCV_H */
