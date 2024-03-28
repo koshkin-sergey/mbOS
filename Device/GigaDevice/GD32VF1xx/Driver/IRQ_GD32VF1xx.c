52cc8833ff017ee5f50bb375fc8c57819b6e1600
Sergey Koshkin <koshkin.sergey@gmail.com>
2024-03-28 11:49:50 +0300

    GD32VF1xx: Interrupt system development
    Added exception vector table.

--- Device/GigaDevice/GD32VF1xx/Driver/IRQ_GD32VF1xx.c
+++ Device/GigaDevice/GD32VF1xx/Driver/IRQ_GD32VF1xx.c
@@ -21,14 +21,73 @@
  *  includes
  ******************************************************************************/
 
+#include <stddef.h>
 #include <asm/gd32vf103xx.h>
 #include <Core/Riscv/irq_riscv.h>
 
+void Default_Handler(void);
+
+/**
+ * @brief Exception Handlers
+ */
+void IAM_Handler(void) __attribute__ ((weak, alias("Default_Handler")));
+void IAF_Handler(void) __attribute__ ((weak, alias("Default_Handler")));
+void ILI_Handler(void) __attribute__ ((weak, alias("Default_Handler")));
+void BKP_Handler(void) __attribute__ ((weak, alias("Default_Handler")));
+void LAM_Handler(void) __attribute__ ((weak, alias("Default_Handler")));
+void LAF_Handler(void) __attribute__ ((weak, alias("Default_Handler")));
+void SAM_Handler(void) __attribute__ ((weak, alias("Default_Handler")));
+void SAF_Handler(void) __attribute__ ((weak, alias("Default_Handler")));
+void ECU_Handler(void) __attribute__ ((weak, alias("Default_Handler")));
+void ECS_Handler(void) __attribute__ ((weak, alias("Default_Handler")));
+void ECM_Handler(void) __attribute__ ((weak, alias("Default_Handler")));
+void IPF_Handler(void) __attribute__ ((weak, alias("Default_Handler")));
+void LPF_Handler(void) __attribute__ ((weak, alias("Default_Handler")));
+void SPF_Handler(void) __attribute__ ((weak, alias("Default_Handler")));
+
+/**
+ * @brief Exception Vector Table
+ */
+static const IRQHandler_t exc_vector[EXCn_MAX_NUM] = {
+  IAM_Handler,    // Instruction address misaligned
+  IAF_Handler,    // Instruction access fault
+  ILI_Handler,    // Illegal instruction
+  BKP_Handler,    // Breakpoint
+  LAM_Handler,    // Load address misaligned
+  LAF_Handler,    // Load access fault
+  SAM_Handler,    // Store/AMO address misaligned
+  SAF_Handler,    // Store/AMO access fault
+  ECU_Handler,    // Environment call from U-mode
+  ECS_Handler,    // Environment call from S-mode
+  NULL,           // Reserved
+  ECM_Handler,    // Environment call from M-mode
+  IPF_Handler,    // Instruction page fault
+  LPF_Handler,    // Load page fault
+  NULL,           // Reserved
+  SPF_Handler     // Store/AMO page fault
+};
+
 /*******************************************************************************
- *  external references
+ *  function implementations (scope: module-local)
  ******************************************************************************/
 
-extern void EXC_Handler(void);
+/**
+ * @brief
+ */
+static void exc_entry(void)
+{
+  uint32_t mcause_val = CSR_READ(CSR_MCAUSE);
+
+  if ((mcause_val & MCAUSE_INT_Msk) == 0U) {
+    IRQHandler_t handler = exc_vector[mcause_val & MCAUSE_CODE_Msk];
+    if (handler != NULL) {
+      handler();
+    }
+  }
+  else {
+
+  }
+}
 
 /*******************************************************************************
  *  function implementations (scope: module-exported)
@@ -40,7 +99,15 @@ extern void EXC_Handler(void);
  */
 int32_t IRQ_Initialize(void)
 {
-  CSR_WRITE(CSR_MTVEC, EXC_Handler);
+  CSR_WRITE(CSR_MTVEC, exc_entry);
 
   return (0);
+}
+
+/**
+ * @brief       Default Handler for Exceptions / Interrupts.
+ */
+void Default_Handler(void)
+{
+  for (;;);
 }
--- Include/Core/Riscv/core_riscv.h
+++ Include/Core/Riscv/core_riscv.h
@@ -476,32 +476,37 @@
 #define CSR_MSCRATCHCSWL              0x349
 #define CSR_MCLICBASE                 0x350
 
-#define MSTATUS_UIE         0x00000001
-#define MSTATUS_SIE         0x00000002
-#define MSTATUS_HIE         0x00000004
-#define MSTATUS_MIE         0x00000008
-#define MSTATUS_UPIE        0x00000010
-#define MSTATUS_SPIE        0x00000020
-#define MSTATUS_UBE         0x00000040
-#define MSTATUS_MPIE        0x00000080
-#define MSTATUS_SPP         0x00000100
-#define MSTATUS_VS          0x00000600
-#define MSTATUS_MPP         0x00001800
-#define MSTATUS_FS          0x00006000
-#define MSTATUS_XS          0x00018000
-#define MSTATUS_MPRV        0x00020000
-#define MSTATUS_SUM         0x00040000
-#define MSTATUS_MXR         0x00080000
-#define MSTATUS_TVM         0x00100000
-#define MSTATUS_TW          0x00200000
-#define MSTATUS_TSR         0x00400000
-#define MSTATUS32_SD        0x80000000
-#define MSTATUS_UXL         0x0000000300000000
-#define MSTATUS_SXL         0x0000000C00000000
-#define MSTATUS_SBE         0x0000001000000000
-#define MSTATUS_MBE         0x0000002000000000
-#define MSTATUS_GVA         0x0000004000000000
-#define MSTATUS_MPV         0x0000008000000000
-#define MSTATUS64_SD        0x8000000000000000
+#define MSTATUS_UIE                   0x00000001
+#define MSTATUS_SIE                   0x00000002
+#define MSTATUS_HIE                   0x00000004
+#define MSTATUS_MIE                   0x00000008
+#define MSTATUS_UPIE                  0x00000010
+#define MSTATUS_SPIE                  0x00000020
+#define MSTATUS_UBE                   0x00000040
+#define MSTATUS_MPIE                  0x00000080
+#define MSTATUS_SPP                   0x00000100
+#define MSTATUS_VS                    0x00000600
+#define MSTATUS_MPP                   0x00001800
+#define MSTATUS_FS                    0x00006000
+#define MSTATUS_XS                    0x00018000
+#define MSTATUS_MPRV                  0x00020000
+#define MSTATUS_SUM                   0x00040000
+#define MSTATUS_MXR                   0x00080000
+#define MSTATUS_TVM                   0x00100000
+#define MSTATUS_TW                    0x00200000
+#define MSTATUS_TSR                   0x00400000
+#define MSTATUS32_SD                  0x80000000
+#define MSTATUS_UXL                   0x0000000300000000
+#define MSTATUS_SXL                   0x0000000C00000000
+#define MSTATUS_SBE                   0x0000001000000000
+#define MSTATUS_MBE                   0x0000002000000000
+#define MSTATUS_GVA                   0x0000004000000000
+#define MSTATUS_MPV                   0x0000008000000000
+#define MSTATUS64_SD                  0x8000000000000000
+
+#define MCAUSE_CODE_Pos               (0U)
+#define MCAUSE_CODE_Msk               (0x7FFFFFFFUL << MCAUSE_CODE_Pos)
+#define MCAUSE_INT_Pos                (31U)
+#define MCAUSE_INT_Msk                (0x1UL << MCAUSE_INT_Pos)
 
 #endif /* __CORE_RISCV_H */
