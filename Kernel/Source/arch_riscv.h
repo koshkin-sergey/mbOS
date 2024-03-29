fe777e4084701fbf50bad17fdd2d234ad72906ce
Sergey Koshkin <koshkin.sergey@gmail.com>
2024-03-29 10:47:37 +0300

    Kernel: Added stack initialization for RISC-V.

--- Kernel/Source/arch_riscv.h
+++ Kernel/Source/arch_riscv.h
@@ -46,6 +46,50 @@ extern uint8_t IRQ_PendSV;
 #define BEGIN_CRITICAL_SECTION        uint32_t mode = CSR_READ_CLEAR(CSR_MSTATUS, MSTATUS_MIE);
 #define END_CRITICAL_SECTION          CSR_WRITE(CSR_MSTATUS, mode);
 
+#define THREAD_INITIAL_MSTATUS        (MSTATUS_MPP_M      | \
+                                       MSTATUS_MPIE       | \
+                                       MSTATUS_FS_INITIAL | \
+                                       MSTATUS_VS_INITIAL)
+
+/*******************************************************************************
+ *  typedefs and structures
+ ******************************************************************************/
+
+typedef struct thread_stack_frame {
+  uint32_t epc;         /* epc - epc    - program counter                     */
+  uint32_t ra;          /* x1  - ra     - return address for jumps            */
+  uint32_t t0;          /* x5  - t0     - temporary register 0                */
+  uint32_t t1;          /* x6  - t1     - temporary register 1                */
+  uint32_t t2;          /* x7  - t2     - temporary register 2                */
+  uint32_t s0_fp;       /* x8  - s0/fp  - saved register 0 or frame pointer   */
+  uint32_t s1;          /* x9  - s1     - saved register 1                    */
+  uint32_t a0;          /* x10 - a0     - return value or function argument 0 */
+  uint32_t a1;          /* x11 - a1     - return value or function argument 1 */
+  uint32_t a2;          /* x12 - a2     - function argument 2                 */
+  uint32_t a3;          /* x13 - a3     - function argument 3                 */
+  uint32_t a4;          /* x14 - a4     - function argument 4                 */
+  uint32_t a5;          /* x15 - a5     - function argument 5                 */
+#ifndef __riscv_32e
+  uint32_t a6;          /* x16 - a6     - function argument 6                 */
+  uint32_t a7;          /* x17 - s7     - function argument 7                 */
+  uint32_t s2;          /* x18 - s2     - saved register 2                    */
+  uint32_t s3;          /* x19 - s3     - saved register 3                    */
+  uint32_t s4;          /* x20 - s4     - saved register 4                    */
+  uint32_t s5;          /* x21 - s5     - saved register 5                    */
+  uint32_t s6;          /* x22 - s6     - saved register 6                    */
+  uint32_t s7;          /* x23 - s7     - saved register 7                    */
+  uint32_t s8;          /* x24 - s8     - saved register 8                    */
+  uint32_t s9;          /* x25 - s9     - saved register 9                    */
+  uint32_t s10;         /* x26 - s10    - saved register 10                   */
+  uint32_t s11;         /* x27 - s11    - saved register 11                   */
+  uint32_t t3;          /* x28 - t3     - temporary register 3                */
+  uint32_t t4;          /* x29 - t4     - temporary register 4                */
+  uint32_t t5;          /* x30 - t5     - temporary register 5                */
+  uint32_t t6;          /* x31 - t6     - temporary register 6                */
+#endif
+  uint32_t mstatus;     /*              - machine status register             */
+} thread_stack_frame_t;
+
 /*******************************************************************************
  *  exported functions
  ******************************************************************************/
@@ -74,43 +118,22 @@ void PendServCallReq(void)
 __STATIC_INLINE
 uint32_t StackInit(StackAttr_t *attr, bool privileged)
 {
-  uint32_t psr;
-//  bool     thumb = (attr->func_addr & 1U) != 0U;
+  (void) privileged;
+  thread_stack_frame_t *frame;
 
-  if (privileged) {
-//    if (thumb) {
-//      psr = CPSR_MODE_SYSTEM | CPSR_T_Msk;
-//    } else {
-//      psr = CPSR_MODE_SYSTEM;
-//    }
-//  } else {
-//    if (thumb) {
-//      psr = CPSR_MODE_USER   | CPSR_T_Msk;
-//    } else {
-//      psr = CPSR_MODE_USER;
-//    }
+  frame = (thread_stack_frame_t *)(attr->stk_mem + attr->stk_size);
+  frame -= sizeof(thread_stack_frame_t);
+
+  for (uint32_t i = 0U; i < sizeof(thread_stack_frame_t) / sizeof(uint32_t); ++i) {
+    ((uint32_t *)frame)[i] = 0xdeadbeef;
   }
 
-  uint32_t *stk = (uint32_t *)(attr->stk_mem + attr->stk_size);
+  frame->a0      = attr->func_param;
+  frame->epc     = attr->func_addr;
+  frame->ra      = attr->func_exit;
+  frame->mstatus = THREAD_INITIAL_MSTATUS;
 
-  *(--stk) = psr;                               //-- xPSR
-  *(--stk) = attr->func_addr;                   //-- Entry Point (PC)
-  *(--stk) = attr->func_exit;                   //-- R14 (LR)
-  *(--stk) = 0x12121212L;                       //-- R12
-  *(--stk) = 0x03030303L;                       //-- R3
-  *(--stk) = 0x02020202L;                       //-- R2
-  *(--stk) = 0x01010101L;                       //-- R1
-  *(--stk) = attr->func_param;                  //-- R0 - thread's function argument
-  *(--stk) = 0x11111111L;                       //-- R11
-  *(--stk) = 0x10101010L;                       //-- R10
-  *(--stk) = 0x09090909L;                       //-- R9
-  *(--stk) = 0x08080808L;                       //-- R8
-  *(--stk) = 0x07070707L;                       //-- R7
-  *(--stk) = 0x06060606L;                       //-- R6
-  *(--stk) = 0x05050505L;                       //-- R5
-  *(--stk) = 0x04040404L;                       //-- R4
-
-  return ((uint32_t)stk);
+  return ((uint32_t)frame);
 }
 
 __STATIC_FORCEINLINE
